c6f065e0085264185a12792d36dd67574a583b40
Brian Angulo <brianangulo96@gmail.com>
2021-09-08 00:37:53 -0500

    Setting up the frontend a bit and begining work on the navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+    <ul class="nav-ul">
+      <li class="nav-li">
+        <a href="">Tracker</a>
+      </li>
+      <li class="nav-li">
+        <a href="">Login</a>
+      </li>
+      <li class="nav-li">
+        <a href="">Logout</a>
+      </li>
+      <li class="nav-li">
+        <a href="">Sign Up</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss">
+.navbar {
+    background-color: lightblue;
+    margin: 0%;
+  .nav-ul {
+    top: 0; 
+    display: flex;
+    justify-content: space-evenly;
+    .nav-li {
+      list-style: none;
+      a {
+        color: black;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
