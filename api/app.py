9b7db8c1d3a869e1a277fbe5591fc77e516e273e
Brian Angulo <brianangulo96@gmail.com>
2021-09-09 01:10:02 -0500

    added resource to app

--- api/app.py
+++ api/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, app
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from resources import HelloFromPy
+from resources import ShoppingList
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
 db = SQLAlchemy(app)
+CORS(app)
 
-api.add_resource(HelloFromPy, '/hello')
+api.add_resource(ShoppingList, '/hello')
 
 if __name__ == "__main__":
     db.create_all()
