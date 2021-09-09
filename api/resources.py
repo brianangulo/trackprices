cc839e2dc8261f6a2a9bfdbb6febe5679c390419
Brian Angulo <brianangulo96@gmail.com>
2021-09-09 01:10:49 -0500

    more resource

--- api/resources.py
+++ api/resources.py
@@ -1,5 +1,33 @@
 from flask_restful import Resource
 
-class HelloFromPy(Resource):
+class ShoppingList(Resource):
     def get(self):
-        return {"hello": "world"}
+        return  [
+            {
+                "name": "bread",
+                "id": "1"
+            },
+            {
+                "name": "milk",
+                "id": "2"
+            },
+            {
+                "name": "coffee",
+                "id": "3"
+            },
+            {
+                "name": "water",
+                "id": "4"
+            },
+            {
+                "name": "rice",
+                "id": "5"
+            }
+        ]
+    def post(self):
+        return {"hello": "hello"}
+
+
+
+
+       
