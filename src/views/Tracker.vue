fc4a8d4713a1b15b16606a7d9cbc7bb58104b60b
Brian Angulo <brianangulo96@gmail.com>
2021-09-09 01:11:04 -0500

    add items logic

--- src/views/Tracker.vue
+++ src/views/Tracker.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="tracker">
     <h1>Track Your Shopping List</h1>
+    <input id="add" ref="add" type="text">
+    <button @click="addItem()">Add Item</button>
     <ul class="shop-list">
-      <li v-for="item, index in list" :key="index" class="item"><input :id="index" type="checkbox" /> {{ item }} </li>
+      <li v-for="item, index in list" :key="index" class="item"><input :id="item.id" type="checkbox" /> {{ item.name }} </li>
     </ul>
   </div>
 </template>
@@ -12,8 +14,37 @@ export default {
   name: "Tracker",
   data() {
     return {
-      list: ["bread", "milk", "water", "coffee"]
+      list: []
     }
+  },
+  methods: {
+   async fetchShoppingList() {
+      const response = await fetch("http://127.0.0.1:5000/hello");
+      this.$data.list = JSON.parse((await response.text()));
+    },
+    saveShoppingItem: async (item) => {
+      const response = await fetch("http://127.0.0.1:5000/hello", {
+        method: "POST",
+        body: JSON.stringify(item)
+      });
+      console.log(response);
+    },
+    addItem() {
+      const inputEl = this.$refs.add;
+      if (inputEl.value) {
+        const tempList = [...this.$data.list];
+        tempList.push({
+          name: inputEl.value,
+          id: Math.floor(Math.random() * 20)
+        });
+        this.$data.list = [...tempList];
+        inputEl.value = "";
+        this.saveShoppingItem()
+      }
+    }
+  },
+  mounted() {
+    this.fetchShoppingList();
   }
 };
 </script>
