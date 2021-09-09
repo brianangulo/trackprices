f05c64c3c4f9cf44332dde28de529ad9d52773b9
Brian Angulo <brianangulo96@gmail.com>
2021-09-08 23:32:34 -0500

    Tracker view built

---
+++ src/views/Tracker.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tracker">
+    <h1>Track Your Shopping List</h1>
+    <ul class="shop-list">
+      <li v-for="item, index in list" :key="index" class="item"><input :id="index" type="checkbox" /> {{ item }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tracker",
+  data() {
+    return {
+      list: ["bread", "milk", "water", "coffee"]
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.tracker {
+  .shop-list {
+    .item {
+      list-style: none;
+    }
+  }
+}
+</style>
